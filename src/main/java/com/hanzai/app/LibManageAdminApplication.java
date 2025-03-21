e52c26b74738a384834dcebf07eb3b4309fdf02b
hanzai <1613710573@qq.com>
2025-03-21 21:53:50 +0800

    init commit

---
+++ src/main/java/com/hanzai/app/LibManageAdminApplication.java
@@ -0,0 +1,13 @@
+package com.hanzai.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibManageAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibManageAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanzai/app/LibManageAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanzai.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibManageAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
